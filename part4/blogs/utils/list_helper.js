1e82f09f26b8c2a07c6a44bc49524862a0901b40
elvis-visi <sivisevi1@gmail.com>
2023-05-18 20:08:30 +0100

    4.1.3. exercise 4.3 helper functions and unit tests.

---
+++ part4/blogs/utils/list_helper.js
@@ -0,0 +1,11 @@
+/* helper functions that are meant to assist in dealing with the blog list. */
+
+//dummy function that receives an array of blog posts as a parameter and always returns the value 1
+
+const dummy = (blogs) => {
+    return 1;
+}
+
+module.exports = {
+    dummy
+  }
