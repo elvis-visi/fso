efe050a8e41a63fd90406ffe0dc0dd2d662dfb66
elvis-visi <sivisevi1@gmail.com>
2023-06-27 12:01:01 +0200

    4.10, exercise 4.9 blogs, verify the existence of  a property called id in the blogs.

--- part4/blogs/tests/blog_api.test.js
+++ part4/blogs/tests/blog_api.test.js
@@ -16,19 +16,39 @@ beforeEach(async () => {
     const blogObjects = helper.initialBlogs
         .map(blog => new Blog(blog))
 
-        const promiseArray = blogObjects.map(blog => blog.save())
-        await Promise.all(promiseArray)
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
 })
 
 test('blogs returned as JSON', async () => {
     await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 
 })
+
+//unique id property of blog posts is named id
+
+
+
+test('id property exists in each blog object', async () => {
+    // call the /api/blogs endpoint
+    const response = await api.get('/api/blogs')
+
+    // check whether there is a response body
+    expect(response.body).toBeDefined()
+
+    // iterate over each blog object in the response body
+    response.body.forEach(blog => {
+        // for each blog object, check if the id property is defined
+        expect(blog.id).toBeDefined()
+    })
+})
+
+
 
 
 afterAll(async () => {
     await mongoose.connection.close()
-  })
+})
